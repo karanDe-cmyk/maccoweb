04ebdb5d317f5c4c0470a891f926e75a9ef98667
Mukultyagi <mukul.tyagi@fitnearn.com>
2025-05-09 13:34:57 +0530

    Your commit message

--- src/app/Component/Footerpage/page.js
+++ src/app/Component/Footerpage/page.js
@@ -16,6 +16,7 @@ import {
   Globe
 } from 'lucide-react';
 
+
 const Footerpage = () => {
   return (
     <footer className="bg-gray-900 text-gray-300 w-full overflow-hidden"> {/* Added overflow-hidden to the main footer */}
