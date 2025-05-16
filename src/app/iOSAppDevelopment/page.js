3ae9b3e21a1afa4f1cc681aa793498863b48c640
karan <147327688+karanDe-cmyk@users.noreply.github.com>
2025-05-16 18:35:04 +0530

    Update page.js

--- src/app/iOSAppDevelopment/page.js
+++ src/app/iOSAppDevelopment/page.js
@@ -78,7 +78,7 @@
 //             viewport={{ once: true }}
 //             className="lg:w-1/2 lg:pl-8"
 //           >
-//             <img
+//             <Image
 //               src="/ios2.png"
 //               alt="iOS App Development"
 //               className="rounded-lg shadow-xl w-full h-[370px]"
@@ -271,7 +271,7 @@ function HeroSection() {
 
               <Link href="/contact">
                 <button className="bg-white cursor-pointer text-indigo-900 font-semibold py-3 px-6 rounded-full hover:bg-indigo-100 transition duration-300">
-                  LET'S DISCUSS
+                  LET&aposS DISCUSS
                 </button>
               </Link>
             </motion.div>
@@ -319,7 +319,7 @@ function HeroSection() {
                       className="bg-white border border-gray-300 px-3 py-2.5 rounded-md cursor-pointer flex items-center justify-between shadow-sm hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
                     >
                       <div className="flex items-center gap-2">
-                        <img
+                        <Image
                           src={countryFlags[formData.countryCode]}
                           alt="Flag"
                           className="w-5 h-5"
@@ -350,7 +350,7 @@ function HeroSection() {
                             onClick={() => handleCountryCodeChange(code)}
                             className="flex items-center gap-2 px-3 py-2 text-sm cursor-pointer hover:bg-gray-100"
                           >
-                            <img src={flagUrl} alt={`${code} flag`} className="w-5 h-5" />
+                            <Image src={flagUrl} alt={`${code} flag`} className="w-5 h-5" />
                             {code}
                           </li>
                         ))}
